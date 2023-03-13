9fae924122649dd335ff7d796608b824005f2016
Amr Abdelkamel <amromoorie@gmail.com>
2023-03-13 16:53:01 +0200

    feat (product repo)

--- server/SimpleWebStoreApp/SimpleWebStore/Repositories/ProductRepository.cs
+++ server/SimpleWebStoreApp/SimpleWebStore/Repositories/ProductRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 using SimpleWebStore.Data;
+using SimpleWebStore.DTOs;
 using SimpleWebStore.Models;
 
 namespace SimpleWebStore.Repositories;
@@ -11,6 +12,61 @@ internal static class ProductRepository
     {
         using var db = new ApplicationDbContext();
         return await db.Products.ToListAsync();
+    }
+
+    //internal async static Task<bool> UpdateProductsQuantityAsync(int productIds, CartDto customerCart)
+    //{
+    //    using var db = new ApplicationDbContext();
+    //   var products=  await db.Products.Where(p => productIds.Equals(productIds)).ToListAsync();
+
+    //    // update product quantities
+    //    products.Select(p =>
+    //    {
+    //        var cartItem = customerCart.CartItems.FirstOrDefault(ci => ci.Id == p.Id);
+    //        if (cartItem != null)
+    //        {
+    //            p.Quantity -= cartItem.Quantity;
+    //        }
+    //        return p;
+    //    }).ToList();
+    //}
+    internal async static Task<(bool success, string message)> UpdateProductsQuantityAsync(IEnumerable<int> productIds, CartDto customerCart)
+    {
+        using var db = new ApplicationDbContext();
+        var products = await db.Products.Where(p => productIds.Contains(p.Id)).ToListAsync();
+
+        var productQuantity = 0;
+        var orderSuceeded = true;
+
+        foreach (var product in products)
+        {
+            var cartItem = customerCart.CartItems.FirstOrDefault(ci => ci.Id == product.Id);
+
+            // return false when there is not sufficient producst or cart item doesnot exist
+            if (cartItem is null && cartItem.Quantity > product.Quantity)
+            {
+                orderSuceeded = false;
+                return (false, $"Insufficient quantity for product with Id: {product.Id}, try again later");
+            }
+
+
+            productQuantity += cartItem.Quantity;
+
+            // decrement product quantity records according per customer items
+            product.Quantity -= cartItem.Quantity;
+        }
+
+        // Undo product quantity records update when order failed
+        if (!orderSuceeded)
+        {
+            foreach (var product in products)
+            {
+                product.Quantity += productQuantity;
+            }
+        }
+
+        await db.SaveChangesAsync();
+        return (true, "Product quantities updated successfully");
     }
 
 }
