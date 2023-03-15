75b2cbdddd71d1ba86702be166f7b61bb0a4e1dc
Amr Abdelkamel <amromoorie@gmail.com>
2023-03-15 23:53:57 +0200

    feat (product service)

---
+++ app-client/src/app/products/product.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { ApiPaths } from '../enums/api-paths';
+import { Product } from '../model/Product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  constructor(private http: HttpClient) {}
+
+  baseUrl = environment.baseUrl;
+
+  getProducts(): Observable<Product> {
+    const url = `${this.baseUrl}/${ApiPaths.Products}`;
+    return this.http.get<Product>(url);
+  }
+}
